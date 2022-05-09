295aa35afbb4041b9bb6bf8879342ab7323038c9
Nicholas Allen <nicallennn@gmail.com>
2022-05-09 11:16:00 +0100

    feat(index.tsx): added auth : non auth pattern to root of app

---
+++ src/AuthApp.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+const AuthApp = () => {
+  return (
+    <div className="App">
+      <h1>FinWiz Auth</h1>
+    </div>
+  );
+};
+
+export default AuthApp;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import AuthApp from 'AuthApp';
 import './index.css';
+import 'antd/dist/antd.min.css';
 
 const root = document.getElementById('root');
+const auth = true;
 
 render(
   <StrictMode>
-    <App />
+    {auth ? <AuthApp /> : <App />}
   </StrictMode>,
   root
 );
