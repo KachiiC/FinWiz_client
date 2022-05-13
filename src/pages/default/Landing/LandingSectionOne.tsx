fe12146c62fb5cbdf9082fef256d6c13ed724f7a
Tom Broad <tombroadyt@gmail.com>
2022-05-13 12:58:10 +0100

    feat: landing page first section

---
+++ src/pages/default/Landing/LandingSectionOne.tsx
@@ -0,0 +1,27 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
+const LandingSectionOne = () => {
+
+  const { user } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <>
+      <div className='landingSectionOneContainer'>
+        <div className='landingSectionOneLeft'>
+          <div>
+            <h1>FinWiz</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea officia optio voluptate dolores quos illum velit.
+            Ea officia optio voluptate dolores quos illum velit aut minus ad.</p>
+          </div>
+          {!user && <button className='landingLoginButton' onClick={loginWithRedirect}>Login</button>}
+        </div>
+        <div className='landingSectionOneRight'>
+          <p>Some cool background objects</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingSectionOne;
